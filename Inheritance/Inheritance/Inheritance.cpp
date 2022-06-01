f1b055ef02779bfc658b50a7811f9a6f1cbc097a
JuliaBelokopytskaya <julia.malezh@yandex.by>
2022-06-01 21:45:09 +0300

    add inheritance

---
+++ Inheritance/Inheritance/Inheritance.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+class Square
+{
+protected:
+	int side;
+public:
+	Square() { side = 0; }
+	Square(int n) { side = n; }
+	int GetCount() { 
+		cout << "Square side"; 
+		return side; 
+	}
+	void SetCount(int n) { side = n; }
+	int SolveSquare(){ return (side * side); }
+	int PerimeterSquare() { return (4 * side); }
+	int DiagonalSquera() { return sqrt(2 * (side * side)); }
+};
+class Cube :public Square
+{
+public:
+
+	Cube() :Square() {};
+	Cube(int n) :Square(n) {};
+	int Volume() { return side ^ 3; }
+	int SolveSquare() { return 6*side^2; }
+	int GetCount() { 
+		cout << "Cube side";
+		return side;
+	}
+	void Set(int n) { Square::SetCount(n); }
+
+};
+int main()
+{
+	const int N1 = 3;
+	const int N2 = 3;
+	Square s[N1];
+	int sum=0, count = 0;
+	int S[N2];
+	for (int i = 0; i < N1; i++)
+		s[i].SetCount(i + N1);
+	for (int i = 0; i < N1; i++) 
+		sum += s[i].SolveSquare();
+	cout << "Square mean=" << float(1.0*sum /N1)<<endl;
+	Cube d[N2];
+	for (int i = 0; i < N2; i++)
+		d[i].Set(i + N2);
+	for (int i = 0; i < N2; i++)
+		S[i] = d[i].SolveSquare();
+	int max = S[0];
+	for (int i = 0; i < N2; i++) {
+		if (S[i] > max)
+			max = S[i];
+	}
+	for (int i = 0; i < N2; i++) {
+		if (S[i] == max)
+			count++;
+	}
+	cout << "Count max square cube=" << count;
+	return 0;
+}
---
+++ Inheritance2/Inheritance2/Inheritance2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base1
+{
+    int b;
+    public:
+        Base1(int z) : b(z){ }
+        void showBase1()
+        {
+            cout << "Class Base1 "<<b<<endl;
+        }
+};
+class Base2
+{
+    int b1;
+    public:
+        Base2(int z) : b1(z) { }
+        void showBase2()
+        {
+            cout << "Class Base2 "<<b1<<endl;
+        }
+};
+class Proizv1 :public Base1 {
+    int p;
+    public:
+        Proizv1(int m, int n) :Base1(m), p(n) {}
+        void showProizv1()
+        {
+            cout << "Class Proizv1 "<<p<<endl;
+            showBase1();
+        }
+};
+class Proizv2 :private Base1 {
+    int p1;
+    public:
+        Proizv2(int m, int n) :Base1(m), p1(n) {}
+        void showProizv2()
+        {
+            cout << "Class Proizv2 "<<p1<<endl;
+            showBase1();
+        }
+};
+class Proizv3 :private Base2, public Proizv1, public Proizv2 {
+    int p2;
+    public:
+    Proizv3(int m, int n, int z, int y) :Base2(m), Proizv1(n,m), Proizv2(z,m), p2(y) {}
+    void showProizv3()
+    {
+        cout << "Class Proizv3 "<<p2<<endl;
+        showBase2();
+        showProizv1();
+        showProizv2();
+    }
+};
+void main() {
+    Proizv3 P(10,20,30,40);
+    cout << "Proizv3 P(10,20,30,40)" << endl;
+    P.showProizv3();
+}
