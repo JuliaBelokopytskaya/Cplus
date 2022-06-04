df0911625f68b8f929bf137498e5f05a1ddb5576
JuliaBelokopytskaya <julia.malezh@yandex.by>
2022-06-05 00:34:16 +0300

    add projects

---
+++ MyClass1/MyClass1/MyClass1.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyMass1 {
+private:
+    float** matrix;
+    int m;
+    int n;
+public:
+    MyMass1();
+    MyMass1(int, int);
+    ~MyMass1();
+    void init();
+    void get();
+    void print();
+    void solve();
+    float max(const MyMass1&);
+};
+
+MyMass1::MyMass1()
+{
+    cout << "Constructor without parametrs";
+    m = n = 0;
+    matrix = nullptr;
+}
+MyMass1::MyMass1(int N, int M) {
+    cout << "Constructor with parametrs";
+    n = N;
+    m = M;
+    matrix = new float* [n];
+    for (int i = 0; i < n; i++)
+        matrix[i] = new float[m];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            matrix[i][j] = 0;
+
+}
+
+MyMass1::~MyMass1() {
+    cout << "Destructor";
+    if (m > 0)
+    {
+        for (int i = 0; i < n; i++)
+            delete[] matrix[i];
+    }
+    if (n > 0)
+        delete[] matrix;
+
+}
+void MyMass1::get() {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+}
+void MyMass1::init() {
+    srand(time(0));
+    int max;
+    int min;
+    cout << "Input random point";
+    cin >> max;
+    cin >> min;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i][j] = rand() % max - min;
+        }
+    }
+}
+float MyMass1::max(const MyMass1& matrix1) {
+    float max = matrix1.matrix[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (max < matrix1.matrix[i][j])
+                max = matrix1.matrix[i][j];
+        }
+    }
+    return max;
+}
+void MyMass1::solve() {
+    float maximum = max(*this);
+    cout << maximum << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i][j] = matrix[i][j] + maximum;
+        }
+    }
+
+}
+
+void MyMass1::print() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "---------------------------" << endl;
+}
+
+
+int main() {
+    int n, m;
+    cout << "Input size matrix:" << endl;
+    cin >> n >> m;
+    MyMass1 Matrix1(n, m);
+    MyMass1 Matrix2(n, m);
+    Matrix1.init();
+    cout << "Input matrix:" << endl;
+    Matrix2.get();
+    cout << "Matrix #1:" << endl;
+    Matrix1.print();
+    cout << "Matrix #2:" << endl;
+    Matrix2.print();
+    Matrix1.solve();
+    Matrix1.print();
+    return 0;
+}
---
+++ MyString/MyString/MyString.cpp
@@ -0,0 +1,121 @@
+﻿#include<iostream>
+using namespace std;
+#include<string.h>
+
+class MyString
+{
+	char* str;
+
+public:
+	MyString();
+	MyString(int);
+	MyString(char*);
+	MyString(const MyString&);
+	~MyString();
+	void Set(void);
+	MyString pushStr(void);
+	bool check(char);
+	void Print();
+};
+MyString::MyString(char* _str)
+{
+	cout << "Constructor with parameter" << endl;
+	if (_str == nullptr) {
+		str = new char[1];
+		str[0] = '\0';
+	}
+	else
+	{
+		str = new char[strlen(_str) + 1];
+		for (int i = 0; i < strlen(_str); i++)
+			str[i] = _str[i];
+		str[strlen(_str)] = '\0';
+	}
+}
+MyString::MyString(int n) {
+	cout << "Constructor with parameter" << endl;
+	if (n) {
+		str = new char[n + 1];
+		str[n] = '\0';
+	}
+	else
+	{
+		str = new char[1];
+		str[0] = '\0';
+	}
+}
+MyString::MyString() :str{ nullptr } {
+	str = new char[1];
+	str[0] = '\0';
+	cout << "Constructor without parameters" << endl;
+}
+MyString::MyString(const MyString& s)
+{
+	cout << "Constructor copy" << endl;
+	str = new char[strlen(s.str) + 1];
+	for (int i = 0; i < strlen(s.str); i++)
+		str[i] = s.str[i];
+	str[strlen(s.str)] = '\0';
+}
+MyString::~MyString()
+{
+	cout << "Destructor" << endl;
+	delete str;
+}
+void MyString::Print()
+{
+	cout << "Print string" << endl;
+	if (strlen(str) > 0) {
+		for (unsigned j = 0; j < strlen(str); j++)
+			cout << str[j];
+	}
+	cout << endl;
+}
+bool MyString::check(char ch)
+{
+	int count = 0;
+	cout << "Check string" << endl;
+	for (int i = 0; i < strlen(str); i++) {
+		if (ch == str[i])
+			count++;
+	}
+	if (count > 1) return false;
+	return true;
+
+}
+void MyString::Set(void) {
+	cout << "Input string" << endl;
+	char* _str = new char[1000];
+	cin >> _str;
+	str = new char[strlen(_str) + 1];
+	for (unsigned j = 0; j < strlen(_str); j++)
+		str[j] = _str[j];
+	str[strlen(_str)] = '\0';
+
+}
+MyString MyString::pushStr(void)
+{
+	cout << "Method create new string" << endl;
+	char* str1;
+	int len = strlen(str);
+	str1 = new char[len];
+	int j = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (check(str[i])) {
+			str1[j] = str[i];
+			j++;
+		}
+	}
+	str1[j] = '\0';
+	MyString newStr(str1);
+	delete[]str1;
+	newStr.Print();
+	return newStr;
+}
+int main()
+{
+	MyString str;
+	str.Set();
+	str.pushStr();
+	return 0;
+}
